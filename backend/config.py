aff013276adf97605d6dc96189c3e68e3adcadde
Ryan Trad <trad.ryan.alan@gmail.com>
2016-04-27 14:29:49 -0400

    added yy crawler

--- backend/config.py
+++ backend/config.py
@@ -11,6 +11,7 @@ TEST_DB_URI = 'mongodb://test:gthealthtest@ds015919.mlab.com:15919/gthealth_test
 ACTIVATION_LINK = 'localhost:4000/#/home/confirmation/{_id}/{token}'
 PROD_ACTIVATION_LINK = 'www.george.gatech.edu/gthealth/#/home/confirmation/{_id}/{token}'
 
+YIK_YAK_ID = 'E635177CFFEEBF2CD43831C603E3F4F5'
 
 keywords = set(['depressed', 'depression', 'suicide', 'suicidal', 'kill',
                     'unhappy', 'counseling', 'counselor', 'psychiatrist',
@@ -20,26 +21,26 @@ keywords = set(['depressed', 'depression', 'suicide', 'suicidal', 'kill',
 secret_key = 'super secret'
 
 SUBREDDITS = [
-            {'name': 'McGill University', 'subreddit': 'mcgill'},
-            {'name': 'Georgia Tech', 'subreddit': 'gatech'},
-            {'name': 'UT Austin', 'subreddit': 'UTAustin'},
-            {'name': 'Penn State University', 'subreddit': 'PennStateUniversity'},
-            {'name': 'Purdue', 'subreddit': 'purdue'},
-            {'name': 'UC Berkeley', 'subreddit': 'berkeley'},
-            {'name': 'CalPoly Ubispo', 'subreddit': 'CalPoly'},
-            {'name': 'UC Santa Barbara', 'subreddit': 'ucsantabarbara'},
-            {'name': 'North Carolina State University', 'subreddit': 'ncsu'},
-            {'name': 'York University', 'subreddit': 'yorku'},
-            {'name': 'Texas A&M', 'subreddit': 'aggies'},
-            {'name': 'Arizona State University', 'subreddit': 'asu'},
-            {'name': 'University of Central Florida', 'subreddit': 'ucf'},
-            {'name': 'University of British Columbia', 'subreddit': 'UBC'},
-            {'name': 'University of Maryland', 'subreddit': 'UMD'},
-            {'name': 'Rochester Institute of Technology', 'subreddit': 'rit'},
-            {'name': 'Ohio State University', 'subreddit': 'OSU'},
-            {'name': 'UC San Diego', 'subreddit': 'ucsd'},
-            {'name': 'University of Missouri', 'subreddit': 'mizzou'},
-            {'name': 'University of Georgia', 'subreddit': 'UGA'}
+            {'name': 'McGill University', 'subreddit': 'mcgill', 'latitude': 45.5048, 'longitude': -73.5772},
+            {'name': 'Georgia Tech', 'subreddit': 'gatech', 'latitude': 33.7756, 'longitude': -84.3963   },
+            {'name': 'UT Austin', 'subreddit': 'UTAustin', 'latitude': 30.2849, 'longitude': -97.7341},
+            {'name': 'Penn State University', 'subreddit': 'PennStateUniversity', 'latitude': 40.7982, 'longitude': -77.8599},
+            {'name': 'Purdue', 'subreddit': 'purdue', 'latitude': 40.4237, 'longitude': -86.9212},
+            {'name': 'UC Berkeley', 'subreddit': 'berkeley', 'latitude': 37.8719, 'longitude': -122.2585},
+            {'name': 'CalPoly Ubispo', 'subreddit': 'CalPoly', 'latitude': 35.3050, 'longitude': -120.6625},
+            {'name': 'UC Santa Barbara', 'subreddit': 'ucsantabarbara', 'latitude': 34.4140, 'longitude': -119.8489},
+            {'name': 'North Carolina State University', 'subreddit': 'ncsu', 'latitude': 35.7847, 'longitude': -78.6821},
+            {'name': 'York University', 'subreddit': 'yorku', 'latitude': 43.7735, 'longitude': -79.5019},
+            {'name': 'Texas A&M', 'subreddit': 'aggies', 'latitude': 30.6191, 'longitude': -96.3359},
+            {'name': 'Arizona State University', 'subreddit': 'asu', 'latitude': 33.4242, 'longitude': -111.9281},
+            {'name': 'University of Central Florida', 'subreddit': 'ucf', 'latitude': 28.6024, 'longitude': -81.2001},
+            {'name': 'University of British Columbia', 'subreddit': 'UBC', 'latitude': 49.2606, 'longitude': -123.2460},
+            {'name': 'University of Maryland', 'subreddit': 'UMD', 'latitude': 38.9869, 'longitude': -76.9426},
+            {'name': 'Rochester Institute of Technology', 'subreddit': 'rit', 'latitude': 43.0845, 'longitude': -77.6764},
+            {'name': 'Ohio State University', 'subreddit': 'OSU', 'latitude': 40.0142, 'longitude': -83.0309},
+            {'name': 'UC San Diego', 'subreddit': 'ucsd', 'latitude': 32.8801, 'longitude': -117.2340},
+            {'name': 'University of Missouri', 'subreddit': 'mizzou', 'latitude': 38.9404, 'longitude': -92.3277},
+            {'name': 'University of Georgia', 'subreddit': 'UGA', 'latitude': 33.9480, 'longitude': -83.3773}
       ]
 
 class ConfigurationManager(object):
--- backend/crawler.py
+++ backend/crawler.py
@@ -1,6 +1,7 @@
 import nltk
 from datetime import datetime
 import praw
+import requests
 from sentiment_analysis import SentimentHelper
 
 import config
@@ -108,13 +109,55 @@ class RedditCrawler(Crawler):
 
 class YikYakCrawler(Crawler):
 
-    def crawl(self):
-        pass
+    def crawl(self, latitude=33.7756, longitude=-84.3963):
+        self._login()
+        posts = [self.post_from_yak(post)
+            for post in self._get_new_yaks(latitude, longitude)]
+        for post in filter(self.classifier.classify, posts):
+            post.save()
 
+    def _login(self):
+        self.header = {
+                'Referer' : 'https://www.yikyak.com/'
+            }
+        params = {
+                'userID' : 'E635177CFFEEBF2CD43831C603E3F4F5'#config.YIK_YAK_ID
+            }
+        pin = requests.post('https://www.yikyak.com/api/auth/initPairing', 
+                data=params, headers=self.header).json()['pin']
+        params = {
+                'countryCode': 'USA',
+                'phoneNumber': '4079378010',
+                'pin': pin
+            }
+        self.auth_token = requests.post('https://www.yikyak.com/api/auth/pair', 
+                data=params, headers=self.header).json()
+        self.header['x-access-token'] = self.auth_token
 
+    def _get_new_yaks(self, latitude, longitude):
+        params = {
+                'userLat': latitude,
+                'userLong': longitude,
+                'lat': latitude,
+                'long': longitude,
+                'myHerd': 0
+            }
+        return requests.get('https://www.yikyak.com/api/proxy/v1/messages/all/new',
+                params=params, headers=self.header)
+
+    @staticmethod
+    def post_from_yak(yak):
+        return models.Post(r_id=yak['messageID'],
+                           source='yikyak',
+                           author = yak['posterID'],
+                           url='yakwith.me/share/R/{}'.format(yak['messageID']),
+                           content=yak['message'],
+                           title=None,
+                           created=datetime.utcfromtimestamp(
+                            yak['gmt']),)
 
 def download_corpus():
-    """ Performs a search for depression related posts on all of our subreddits"""
+    """ Performs a search for depression related posts on all of our campuses"""
     r = praw.Reddit(user_agent='gthealth')
     for school in config.SUBREDDITS:
         print 'Searching posts from %s' % (school['name'])
@@ -124,11 +167,11 @@ def download_corpus():
         print 'found %d results' % (len(samples))
         for sample in samples:
             sample.save()
-    return
 
 
 if __name__ == '__main__':
     RedditCrawler(SimpleClassifier()).crawl()
+    YikYakCrawler(SimpleClassifier()).crawl()
     # download_corpus()
     # samples = models.Sample.objects(label__exists=True)
     # print test_classifier(SimpleClassifier(), samples)
